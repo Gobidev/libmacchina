ab0510c88b07c004737bddc4a8313a56fa22ee15
Adrian Groh <adrian.groh@t-online.de>
2024-05-09 14:23:48 +0200

    print all

--- examples/test.rs
+++ examples/test.rs
@@ -4,7 +4,8 @@ fn main() {
     use libmacchina::traits::MemoryReadout as _;
     let memory_readout = MemoryReadout::new();
 
-    let total_mem = memory_readout.total().unwrap();
-    let used_mem = memory_readout.used().unwrap();
-    println!("{used_mem} kB / {total_mem} kB");
+    dbg!(memory_readout.total().unwrap());
+    dbg!(memory_readout.free().unwrap());
+    dbg!(memory_readout.reclaimable().unwrap());
+    dbg!(memory_readout.used().unwrap());
 }
